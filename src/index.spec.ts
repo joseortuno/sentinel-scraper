96ccb00691e6c6a46bfe544c9457317b0bbe0fc8
Felipe <felipefcor@gmail.com>
2020-04-19 21:01:58 +0200

    added tests for Scraper methods and reviewed some existent

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -27,7 +27,8 @@ describe("Scraper - Scraping tool", () => {
 
     describe("select method", () => {
       let heman;
-
+      let expected: string = "toy.php?id=1469";
+      
       before(() => {
         heman = new Scraper(
           "https://www.he-man.org/collecting/toycollection.php?id=1"
@@ -39,7 +40,7 @@ describe("Scraper - Scraping tool", () => {
           return item.children.item(1).children.item(0).href;
         });
 
-        expect(selector).to.exist;
+        expect(selector[0]).equals(expected);
       });
 
       it("Should iterate over the item to be able to scraped", () => {
@@ -51,11 +52,20 @@ describe("Scraper - Scraping tool", () => {
           data[index] = [item.children.item(1).children.item(0).href];
         });
 
-        expect(data).to.exist;
+        expect(Object.values(data)[0][0]).equals(expected);
       });
+
+      it("Should do anything when selector does not exist", () => {
+        const selector = heman.select(".tblrowwwww", (item) => {
+          return item.children.item(1).children.item(0).href;
+        });
+        expect(selector).to.have.lengthOf(0);
+       
+      });
+
     });
 
-    describe("static for method", () => {
+    describe.only("static for method", () => {
       let heman,
         urls,
         completeUrls = [];
@@ -77,7 +87,7 @@ describe("Scraper - Scraping tool", () => {
         });
       });
 
-      it("Should go throuth an array of urls ", () => {
+      it("Should go through an array of urls ", () => {
         const data = [];
         const selectUrls = completeUrls.slice(0, 5);
 
@@ -99,132 +109,19 @@ describe("Scraper - Scraping tool", () => {
 
         expect(data).to.exist;
       });
-
-      it("Should work by concatenating the url", () => {
-        const data = [];
-        const selectUrls = urls.slice(0, 5);
-        Scraper.for(selectUrls, (scraper) => {
-          const detail = scraper.select("#collect_rightcenter", (item) => {
-            return item.children
-              .item(0)
-              .children.item(1)
-              .children.item(0)
-              .children.item(0)
-              .children.item(0)
-              .children.item(0)
-              .children.item(0)
-              .children.item(0)
-              .children.item(1).textContent;
-          });
-          data.push(detail);
-        });
-
-        expect(data).to.exist;
-      });
+     
 
       it("Should return the parameters index and return url", () => {
-        const selectUrls = urls.slice(0, 5);
-        const expectUrls = completeUrls.slice(0, 5);
+        const selectUrls = completeUrls.slice(0, 5);
+        
         Scraper.for(selectUrls, (scraper, index, url) => {
           expect(scraper instanceof Object).to.be.true;
           expect(index).to.exist;
-          expect(expectUrls[index]).to.equal(url);
+          expect(selectUrls[index]).to.equal(url);
         });
       });
     });
   });
 
 
-  // it("Scraper - method selector - Should return error when not insert parameter", () => {
-  //   expect(() => {
-  //     const heman = new Scraper(
-  //       "https://www.he-man.org/collecting/toycollection.php?id=1"
-  //     );
-  //     heman.select();
-  //   }).to.throw(
-  //     ReferenceError,
-  //     "The method select need parameters: selector and expression."
-  //   );
-  // });
-
-  // it("Scraper - method selector - parameter: SELECTOR. Should return error when not insert a string ", () => {
-  //   expect(() => {
-  //     const heman = new Scraper(
-  //       "https://www.he-man.org/collecting/toycollection.php?id=1"
-  //     );
-  //     heman.select(true);
-  //   }).to.throw(TypeError, "Selector expect a string. true is a boolean.");
-  // });
-
-  // it("Scraper - method selector - parameter: EXPRESSION. Should return error when not insert a string ", () => {
-  //   expect(() => {
-  //     const heman = new Scraper(
-  //       "https://www.he-man.org/collecting/toycollection.php?id=1"
-  //     );
-  //     heman.select(".tblrow");
-  //   }).to.throw(ReferenceError, "The method select need a expression.");
-  // });
-
-  // it("Scraper - method selector - parameter: EXPRESSION. Should return error when not insert a function", () => {
-  //   expect(() => {
-  //     const heman = new Scraper(
-  //       "https://www.he-man.org/collecting/toycollection.php?id=1"
-  //     );
-  //     heman.select(".tblrow", true);
-  //   }).to.throw(
-  //     TypeError,
-  //     "Expression expect a function. true is a boolean."
-  //   );
-  // });
-
-  // it("Scraper - method selector - Should return error when not insert parameters", () => {
-  //   expect(() => {
-  //     Scraper.for();
-  //   }).to.throw(
-  //     ReferenceError,
-  //     "The static method for need parameters: urls and expression."
-  //   );
-  // });
-
-  // it("Scraper - method static for - parameter: URLS. Should return error when not insert an array", () => {
-  //   expect(() => {
-  //     Scraper.for(true);
-  //   }).to.throw(TypeError, "Urls expect a array. true is a boolean.");
-  // });
-
-/*   it("Scraper - method static for - parameter: URLS. Should return error when insert an empty array", () => {
-    expect(() => {
-      Scraper.for([], () => {});
-    }).to.throw(ReferenceError, `There aren't elements in the array urls.`);
-  });
-
-  it("Scraper - method static for - parameter: URLS. Should return error when not insert an array with url", () => {
-    expect(() => {
-      Scraper.for(["error"], () => {});
-    }).to.throw(TypeError, "The element 0 of the array is not a url.");
-  }); */
-
-  // it("Scraper - method selector - parameter: EXPRESSION. Should return error when not insert expression", () => {
-  //   expect(() => {
-  //     Scraper.for(new Array("1"));
-  //   }).to.throw(ReferenceError, "The static method for need a expression.");
-  // });
-
-  // it("Scraper - method static for - parameter: EXPRESSION. Should return error when not insert a function", () => {
-  //   expect(() => {
-  //     Scraper.for(new Array("1"), true);
-  //   }).to.throw(
-  //     TypeError,
-  //     "Expression expect a function. true is a boolean."
-  //   );
-  // });
-
-  // it("Scraper - method static for - parameter: COMPLETE URL. Optional parameter. Should return error when not insert a url with its fomat", () => {
-  //   expect(() => {
-  //     Scraper.for(new Array("1"), () => {}, true);
-  //   }).to.throw(
-  //     ReferenceError,
-  //     "The url to concatenate is not a url format."
-  //   );
-  // });
 });
